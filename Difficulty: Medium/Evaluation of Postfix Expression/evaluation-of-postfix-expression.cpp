ef828620736018c31906b04192db249b7b8abae5
Nikita Motwani <156094156+mnikita23@users.noreply.github.com>
2025-02-28 12:36:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Evaluation of Postfix Expression/evaluation-of-postfix-expression.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int evaluate(vector<string>& arr) {
+        stack<int> st;
+        for(auto z:arr){
+            if(z=="+" || z=="-" || z=="*" || z=="/"){
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+                if(z=="+")
+                    st.push(a+b);
+                else if(z=="-")
+                    st.push(a-b);
+                else if(z=="*")
+                    st.push(a*b);
+                else
+                    st.push(a/b);
+            }
+            else
+                st.push(stoi(z));
+        }
+        return st.top();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string str;
+    getline(cin, str);
+    int t = stoi(str);
+    while (t--) {
+        getline(cin, str);
+        stringstream ss(str);
+        vector<string> arr;
+        while (ss >> str) {
+            arr.push_back(str);
+        }
+        Solution ob;
+        cout << ob.evaluate(arr) << endl;
+        cout << "~" << endl;
+    }
+}
+// } Driver Code Ends
